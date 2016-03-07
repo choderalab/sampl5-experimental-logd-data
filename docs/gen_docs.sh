77dffe136b38909ce3e9ea47ab52b0b843b0856b
Bas Rustenburg <bas.rustenburg@choderalab.org>
2016-03-07 16:53:40 -0500

    Update bootstrap model and documentation.

--- docs/gen_docs.sh
+++ docs/gen_docs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Generate documentation for scripts
 # Depends on docco.
-docco -l linear ../raw_data/process_data.py -o ./
-docco -l linear ../analysis.py -o ./
+docco -l linear --template docco_math.jst -c public/css/docco.css ../raw_data/process_data.py -o ./
+docco -l linear --template docco_math.jst -c public/css/docco.css  ../analysis.py -o ./
+docco --template docco_math.jst -c public/css/docco.css ../wip_code/bootstrap_model.py -o ./
--- wip_code/bootstrap_model.py
+++ wip_code/bootstrap_model.py
@@ -1,188 +1,328 @@
+# Model definitions
+# =================
+# First we define all the models needed to describe the experiments
+
+# List of imported libraries
+from __future__ import print_function
 from numpy.random import normal, choice
 import numpy as np
 import pandas as pd
-import seaborn as sns
+from scipy.interpolate import interp1d
+from uncertainties import ufloat
 
+# Pipette models
+# --------------
+# These models simply calculate the expected imprecision and inaccuracy of the pipettes used for sample preparation.
 
-def cyclohexane_dilution_bootstrap(expected_chx_volume=10., expected_oct_volume=90.):
+# Model of a multichannel pipette, which was used to pipette octanol
+
+def rainin_multichannel_pipetting_model(volume):
+    """ Data obtained from 
+        https://www.shoprainin.com/Pipettes/Multichannel-Manual-Pipettes/Pipet-Lite-XLS%2B/Pipet-Lite-Multi-Pipette-L8-200XLS%2B/p/17013805        
+        
+        Parameters
+        ----------
+        volume - volume pipetted in microliters
+        
+        Notes
+        -----
+        This is the pipette used for pipetting octanol for the cyclohexane dilution into octanol.
+        
+        Returns
+        -------
+        Expected Inaccuracy, Imprecision
     """
-    Bootstrapping model for cyclohexane dilution.
+    imprecision_function = interp1d(
+        [20.0, 100.0, 200.0], # volume range (uL)
+        [0.01, 0.0025,0.0015]) # relative imprecision for these volumes from rainin website
+    
+    inaccuracy_function = interp1d(
+        [20.0, 100.0, 200.0], # volume range (uL)
+        [0.025, 0.008, 0.008]) # relative inaccuracy for these volumes from rainin website
+    
+    return [inaccuracy_function(volume), imprecision_function(volume)]    
+
+
+# Model of a single channel pipette, which was used to pipette cyclohexane
+
+def rainin_singlechannel_pipetting_model(volume):
+    """ Data obtained from
+        https://www.shoprainin.com/Pipettes/Single-Channel-Manual-Pipettes/RAININ-Classic/Rainin-Classic-Pipette-PR-10/p/17008649
+        
+        Parameters
+        ----------
+        volume - volume pipetted in microliters
+        
+        Notes
+        -----
+        This is the pipette used for pipetting cyclohexane into octanol
+           
+        Returns
+        -------
+        Expected Inaccuracy, Imprecision
+        
+    """
+    imprecision_function = interp1d(
+        [1.0, 5.0, 10.0], # volume range (uL)
+        [0.012, 0.006, 0.004]) # relative imprecision for these volumes from rainin website
+    
+    inaccuracy_function = interp1d(
+        [1.0, 5.0, 10.0], # volume range (uL)
+        [0.025, 0.015, 0.01]) # relative inaccuracy for these volumes from rainin website
+    
+    return [inaccuracy_function(volume), imprecision_function(volume)]    
+
+
+# Bootstrap models
+# ----------------
+# These functions simulate new, random values of the experiment, based on the measured estimates, and expected bias and variance.
+
+def cyclohexane_dilution_bootstrap(expected_cyclohexane_volume=10., expected_octanol_volume=90.):
+    """
+    Bootstrapping model for cyclohexane dilution dilution step.
 
     Parameters
     ----------
-    expected_chx_volume - the expected volume of cyclohexane
-    expected_oct_volume - the expected volume of octanol that was added in order to dilute cyclohexane
+    expected_cyclohexane_volume - float
+        the expected volume of cyclohexane in uL
+    expected_octanol_volume - float
+        the expected volume of octanol that was added in order to dilute cyclohexane in uL        
+  
 
     Returns
     -------
-
+    The expected dilution factor of the cyclohexane sample.
+    
     """
-    chx_inaccuracy = 0.1
-    chx_imprecision = 0.1
-    oct_inaccuracy = 0.05
-    oct_imprecision = 0.05
-
-    # We allow the relative bias to be of different scale between the different volumes pipetted,
-    # but must be same direction.
-
-    # Draw a bias for the chx pipette
-    bias_chx = normal() # draw random normal variate that will scale inaccuracy to determine relative bias
-    # draw a bias for the octanol pipette
-    bias_oct = normal()
-    # Initialize storage for actual concentrations and volumes.
-
-    # Fill initial well of with appropriate dilution of stock solution.
-    # TODO do we by definition use same bias for both?
-    Vchx_actual = expected_chx_volume * ((1 + chx_inaccuracy * bias_chx) + chx_imprecision * normal())
-    Voct_actual = expected_oct_volume * ((1 + oct_inaccuracy * bias_oct) + oct_imprecision * normal())
-
-    return Vchx_actual / (Vchx_actual+Voct_actual)
 
 
-def measurement_bootstrap(chx_signal, buffer_signal):
+       
+    cyclohexane_inaccuracy, cyclohexane_imprecision = rainin_singlechannel_pipetting_model(expected_cyclohexane_volume)       
+    oct_inaccuracy, oct_imprecision = rainin_multichannel_pipetting_model(expected_octanol_volume)
+    
+    # Relative bias and variance are simulated from standard normal distribution
+    # $$\begin{align}
+    # \text{Relative bias} \sim \mathcal{N}(0,1) \\\
+    # \text{Relative variance} \sim \mathcal{N}(0,1) \\\\
+    # \end{align}$$
+    
+    bias_cyclohexane = normal()    
+    bias_octanol = normal()
+    variance_cyclohexane = normal()
+    variance_octanol = normal()    
+     
+    #  Relative variance and bias estimates are scaled by the factor presumed from the pipette model.     
+    Vcyclohexane_sample = expected_cyclohexane_volume * ((1 + cyclohexane_inaccuracy * bias_cyclohexane) + cyclohexane_imprecision * variance_cyclohexane)
+    Voct_sample = expected_octanol_volume * ((1 + oct_inaccuracy * bias_octanol) + oct_imprecision * variance_octanol)
+
+    return Vcyclohexane_sample / (Vcyclohexane_sample+Voct_sample)
+
+
+def measurement_bootstrap(cyclohexane_signal, buffer_signal):
     """
-    Bootstrapping model for errors in signal integration
+    Bootstrapping model for errors due to noise and integration errors in MRM signal.
+    
     Parameters
     ----------
-    signal
+    cyclohexane_signal - int
+        The raw counts for the cyclohexane phase
+    buffer_signal - int    
+        The raw counts for the buffer phase
 
     Returns
     -------
-
+    Random sample of counts cyclohexane, buffer
     """
-    signal_inaccuracy = 0.1
+        
+    # Assumption: the integration procedure is messy, the data is noisy, but less or equal to 10% inaccuracy/imprecision is introduced for these causes. 
+    signal_inaccuracy = 0.1 
     signal_imprecision = 0.1
+   
+    # Relative bias and variance are simulated from standard normal distribution
+    
+    # $$\begin{align}
+    # \text{Relative bias} \sim \mathcal{N}(0,1) \\\\
+    # \text{Relative variance} \sim \mathcal{N}(0,1) \\\\
+    # \end{align}$$
+    
+    # Data is integrated separately, and injections may have different sources of noise, so the random bias is selected separately.
+    
+    bias_cyclohexane = normal()
+    bias_buffer = normal()        
+    variance_cyclohexane = normal()
+    variance_buffer = normal()
+    
+    #  Relative variance and bias estimates are scaled by the factor presumed above     
+    cyclohexane_signal_sample = cyclohexane_signal * ((1 + signal_inaccuracy * bias_cyclohexane) + signal_imprecision * variance_cyclohexane)
+    buffer_signal_sample = buffer_signal * ((1 + signal_inaccuracy * bias_buffer) + signal_imprecision * variance_buffer)
+    
+    return cyclohexane_signal_sample, buffer_signal_sample
 
-    bias_chx = normal()
-    bias_buffer = normal()
-    chx_signal_actual = chx_signal * ((1 + signal_inaccuracy * bias_chx) + signal_imprecision * normal())
-    buffer_signal_actual = buffer_signal * ((1 + signal_inaccuracy * bias_buffer) + signal_imprecision * normal())
-    return chx_signal_actual, buffer_signal_actual
+# Sampling tools
+# -------------
 
+# These functions sample from the data using the models we've defined above.
 
 def resample_repeats(measurements):
+    """
+    Make random selections with replacement from the provided array.
+    
+    Used to resample repeat experiments. Returns array with the same number of repeat measurements as input. 
+    """
     n = measurements.shape[0]
     return measurements[choice(n,n)]
 
 
 def resample_replicates(measurements):
+    """
+    Make random selections with replacement from the provided array, then proceed to do the same for the array inside of the array.    
+    Used to resample repeat experiments, and within each repeat, resample the replicates.    
+    Returns array with the same number of repeat measurements as input. 
+    """
+    
+    # First, select repeats with replacement.
     measurements = resample_repeats(measurements)
+    
+    # For every repeat, resample the replicate measurements.
     for m, meas in enumerate(measurements):
         n = meas.shape[0]
         measurements[m] = meas[choice(n,n)]
     return measurements
 
-
+# This function brings it all together.
 def sample(measurements, resample_measurements=False):
     """
-
+    Sample a result for a single compound, given all its repeats, and technical replicate measurements.
+    
     Parameters
-    ----------
-    buffer_injection_volume
-    chx_injection_volume
-    resample_measurements - bool
-        Resample real data values
-    measurements - numpy array
+    ----------    
+    
+    measurements - np.ndarray
         All measurements for a single compound
         [
-            [repeat 1  [chx_signal, chx_volume, buffer_signal, buffer_volume]_1, ..., [chx_signal, chx_volume, buffer_signal, buffer_volume]_nrepl],
-            [repeat 2  [chx_signal, chx_volume, buffer_signal, buffer_volume]_1, ..., [chx_signal, chx_volume, buffer_signal, buffer_volume]_nrepl],
+            [repeat 1  [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_1, ..., [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_nrepl],
+            [repeat 2  [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_1, ..., [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_nrepl],
             ...
-            [repeat n [chx_signal, chx_volume, buffer_signal, buffer_volume]_1, ..., [chx_signal, chx_volume, buffer_signal, buffer_volume]_nrep]
+            [repeat n [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_1, ..., [cyclohexane_signal, cyclohexane_volume, buffer_signal, buffer_volume]_nrep]
         ]
-
+    resample_measurements - bool, optional
+        Resample from real data values (default: False)
+    
     Returns
     -------
-    Log D - float
+    Log D - array
     """
-
-    # Need ndarray for this to work
+    
+    # This part validates the input structure
+    
+    # Need np.ndarray for this to work
     assert type(measurements) == np.ndarray
 
-    # Make sure data exists as quartets
+    # Make sure data points exists as quartets, otherwise something is wrong.
     assert measurements.shape[-1] == 4
 
+    # To account for numeric bias introduced from the limited amount of measurements
+    # we can choose to sample from them with replacement.
     if resample_measurements:
         measurements = resample_replicates(measurements)
 
     result = np.empty(measurements.size / measurements.shape[-1])  # Number of log D estimates/measurements
+    
+    # First, loop 
     for r, repeat in enumerate(measurements):
         # Every replicate will have the same dilution factor
         actual_dilution_factor =  cyclohexane_dilution_bootstrap()
         for i, measurement in enumerate(repeat):
             chx = measurement[0]
-            chx_injection_volume = measurement[1]
+            cyclohexane_injection_volume = measurement[1]
             buffer = measurement[2]
             buffer_injection_volume = measurement[3]
 
-            # Randomly sample bias and imprecisions in signal
-            # Assumption is that the bias in each measurement is independent,
-            # because the integration is done independently of the other measurements.
-            actual_chx_signal, actual_buffer_signal = measurement_bootstrap(chx,buffer)
-            actual_chx_signal /= actual_dilution_factor
-            proportional_chx_concentration = (actual_chx_signal / chx_injection_volume)
+            # Randomly sample inaccuracy and imprecision in signal           
+            actual_cyclohexane_signal, actual_buffer_signal = measurement_bootstrap(chx,buffer)  
+            actual_cyclohexane_signal /= actual_dilution_factor
+            
+            # Convert m/z counts to concentration.             
+            proportional_cyclohexane_concentration = (actual_cyclohexane_signal / cyclohexane_injection_volume)            
             proportional_buffer_concentration = (actual_buffer_signal / buffer_injection_volume)
-            index = r * len(repeat) + i
-            result[index] = np.log10(proportional_chx_concentration / proportional_buffer_concentration)
-
+            
+            # Get the correct linear index of the measurement.
+            index = r * len(repeat) + i            
+            # Calculate the log D, base 10 as log ([MRM_cyclohexane/vol_cyclohexane]/[MRM_buffer/vol_buffer])
+            result[index] = np.log10(proportional_cyclohexane_concentration / proportional_buffer_concentration) 
 
     return result
 
 
-# This part is run when this script is executed directly
+# Main script
+# ===========
+# Actual execution of the code takes place here.
+
 
 if __name__ == "__main__":
 
 
-
+    # Constructing input arrays
+    # -------------------------
+    
     # Input data from the preprocessing step.
+    
     table = pd.read_excel('processed.xlsx', sheetname='Filtered Data')
 
-    # Multiply all cyclohexane volumes by 10 to remove implicit dilution factor
-    # As a quick fix, they're easily identified in this table as being smaller than 1 microliter),
+    # Multiply all cyclohexane volumes by 10 to remove implicit dilution factor, which we will replace by a bootstrap model of the dilution
+    # As a quick fix, they're easily identified in this table as being smaller than 1 microliter)
     table["Volume"] = table["Volume"].apply(lambda x: x * 10.0 if x < 0.99 else x)
 
     # Store data as convenient arrays in dictionary by compound name
     data_dict = dict()
-    # Every individual repeat experiment (completely independent)
-    for compound, compound_table in table.groupby("Sample Name"):
-        dataset = list()
-        for (dset, repeat), repeat_table in compound_table.groupby(["Set", "Repeat"]):
-            dataset.append(list())
-            for replicate ,replicate_table in repeat_table.groupby("Replicate"):
 
+    # Every compound in the set gets its own list of repeat experiments
+    for compound, compound_table in table.groupby("Sample Name"):        
+        dataset = list()
+        
+        # Every independent repeat gets a list of replicate measurements of that same repeat
+        for (dset, repeat), repeat_table in compound_table.groupby(["Set", "Repeat"]):            
+            dataset.append(list())
+            
+            # Every replicate has two data points, one for each solvent
+            for replicate ,replicate_table in repeat_table.groupby("Replicate"):
                 solvents = replicate_table.groupby(["Solvent"])
                 chx = solvents.get_group('CHX')
                 buffer = solvents.get_group('PBS')
-                chx_signal = float(chx["Analyte Peak Area (counts)"])
-                chx_injection_volume = float(chx["Volume"])
+                
+                # We need both the signal and the volume to calculate the log D
+                cyclohexane_signal = float(chx["Analyte Peak Area (counts)"])
+                cyclohexane_injection_volume = float(chx["Volume"])
+                
                 buffer_signal = float(buffer["Analyte Peak Area (counts)"])
                 buffer_injection_volume = float(buffer["Volume"])
-                dataset[-1].append([chx_signal, chx_injection_volume, buffer_signal, buffer_injection_volume])
+                
+                dataset[-1].append([cyclohexane_signal, cyclohexane_injection_volume, buffer_signal, buffer_injection_volume])  
 
-        # Final structure of each compounds array
-        # [
-        #  [repeat 1 [chx_signal,chx_volume,buffer_signal,buffer_volume]_1, .., [chx_signal,chx_volume,buffer_signal,buffer_volume]_nrepl],
-        #  [repeat 2 [chx_signal,chx_volume,buffer_signal,buffer_volume]_1, .., [chx_signal,chx_volume,buffer_signal,buffer_volume]_nrepl],
-        #  ...
-        #  [repeat n [chx_signal,chx_volume,buffer_signal,buffer_volume]_1, .., [chx_signal,chx_volume,buffer_signal,buffer_volume]_nrepl]
-        # ]
-
+        # We store each set of measurements for the same compound in this dictionary, after converting it to a np.ndarray.
         data_dict[compound] = np.asarray(dataset)
 
-    # The good part starts here
+    # Sampling code
+    # -------------
 
-
-    # Number of samples per compound
-    n_samples = 100
-
+    # Number of bootstrap samples per compound
+    n_samples = 10000
+    
+    # See file below for the results.
+    bootstrap_results = open("LogD_bootstrap.txt", "w")
+    
+    
+    print(" Compound Log(D) +/-", file=bootstrap_results)
     for compound in sorted(data_dict.keys()):
-        samples = [-10000] * n_samples
+        
+        samples = [None] * n_samples # Preallocate list 
         for i in range(n_samples):
-
+            # Draw a new random set of log D estimates
             samples[i] = sample(data_dict[compound], resample_measurements=True)
+        
+        logd = ufloat(np.average(samples),  np.std(samples))
+        # Data is printed with one significant digit in the uncertainty, and the mean is rounded with respect to that digit.
+        print(compound, "{:.1u}".format(logd) , file=bootstrap_results)
 
-        print(compound, np.average(samples) , np.std(samples))
 
-
-
+            
